0a3a7bb248dd2e2d8bd1069a56feb287f1ce8806
MariusP <popazyme@gmail.com>
2018-04-10 12:08:12 -0400

    adding bunch of python

---
+++ draw.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 10:24:49 2018
+
+@author: popaz
+"""
+
+import turtle
+
+painter = turtle.Turtle()
+
+for _ in range(18):
+    painter.forward(100)
+    painter.left(100)
+
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 10:49:24 2018
+
+@author: popaz
+"""
+
+import turtle
+painter = turtle.Turtle()
+for _ in range(3):
+ painter.forward(100)
+ painter.left(120)
+turtle.done()
